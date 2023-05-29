eb5f9ba7c220ee4ba3f6b04e976442e3bd154ffa
Nikolajml <nicolas.maliavko@gmail.com>
2023-05-29 19:30:51 +0300

    Add project files.

---
+++ TMS_Soucedemo/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TMS_Soucedemo
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
