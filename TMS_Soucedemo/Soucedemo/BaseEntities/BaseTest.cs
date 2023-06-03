66b207170b1676e2ef1ce6d6cd43524e85346226
Nikolajml <nicolas.maliavko@gmail.com>
2023-06-03 20:32:03 +0300

    Created Allure HW

--- TMS_Soucedemo/Soucedemo/BaseEntities/BaseTest.cs
+++ TMS_Soucedemo/Soucedemo/BaseEntities/BaseTest.cs
@@ -7,9 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TMS_Soucedemo.Soucedemo.Pages;
+using NUnit.Allure.Core;
+using Allure.Commons;
 
 namespace TMS_Soucedemo.Soucedemo.BaseEntities
 {
+    [AllureNUnit]
     public class BaseTest
     {
         public static readonly string? BaseUrl = Configurator.AppSettings.URL;
@@ -18,10 +21,14 @@ namespace TMS_Soucedemo.Soucedemo.BaseEntities
         protected WaitService? WaitService;
         public LoginPage LoginPage { get; set; }
         public CheckoutCompletePage CheckoutCompletePage { get; set; }
+        
+        private AllureLifecycle _allure;
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
         {
+            //Initialization allure
+            _allure = AllureLifecycle.Instance;
         }
 
         [SetUp]
@@ -38,6 +45,16 @@ namespace TMS_Soucedemo.Soucedemo.BaseEntities
         [TearDown]
         public void TearDown()
         {
+            //Проверка, что тест упал
+            if (TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Failed)
+            {
+                //Создать скриншот
+                Screenshot screenshot = ((ITakesScreenshot)Driver).GetScreenshot();
+                byte[] screenshotBytes = screenshot.AsByteArray;
+
+                //Приклепление скриншота
+                _allure.AddAttachment(name: "Screenshot", type: "image/png", screenshotBytes);
+            }
             Driver?.Quit();
         }
     }
--- TMS_Soucedemo/Soucedemo/Tests/NegativeTest.cs
+++ TMS_Soucedemo/Soucedemo/Tests/NegativeTest.cs
@@ -1,4 +1,6 @@
-﻿using Core.Models;
+﻿using Allure.Commons;
+using Core.Models;
+using NUnit.Allure.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,16 @@ namespace TMS_Soucedemo.Soucedemo.Tests
 {
     public class NegativeTest : BaseTest
     {
-        [Test]
+        [Test(Description = "Unsuccessful login")]
+        [Description("Description of the test")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureOwner("standard_user")]
+        [AllureSuite("PassedSuite")]
+        [AllureSubSuite("GUI")]
+        [AllureIssue("TMS-12")]
+        [AllureTms("TMS-13")]
+        [AllureTag("Smoke")]
+        [AllureLink("https://www.saucedemo.com/")]
         public void UnsuccessfulLoginTest()
         {
             string expectedErroreMessage = "Epic sadface: Username and password do not match any user in this service";
